7746bac0267686d73f18f19f9ae1377a4739aeed
SyBines <seymourb5@nycstudents.net>
2025-05-03 17:49:02 -0400

    Moved To Git. Finished

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    //test cases
+    public static void main(String[] args)
+    {
+        Review[] reviews =
+                {
+                        new Review(4, "Good! Thx"),
+                        new Review(3, "Ok site"),
+                        new Review(5, "Great!"),
+                        new Review(2, "Poor! Bad"),
+                        new Review(3, ""),
+                };
+        ReviewAnalysis r = new ReviewAnalysis(reviews);
+
+        System.out.println(r.getAverageRating());
+        System.out.println(r.collectComments());
+    }
+}
